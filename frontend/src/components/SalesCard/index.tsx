e4f369f06505f9c94d411cb8c19b47da5d382b0a
zm1hawk <vinicius6.lopes6@gmail.com>
2022-11-19 20:13:14 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -5,6 +5,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import { Sale } from "../../models/sale";
 import { BASE_URL } from "../../utils/request";
 import NotificationButton from "../NotificationButton";
+
 import "./styles.css";
 
 function SalesCard() {
@@ -21,9 +22,7 @@ function SalesCard() {
     const dmin = minDate.toISOString().slice(0,10);
     const dmax = maxDate.toISOString().slice(0,10);
     
-    console.log(dmin)
-    
-    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
+   axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
       setSales(response.data.content);
     });
   }, [minDate, maxDate]);
@@ -75,7 +74,7 @@ function SalesCard() {
                   <td>R${sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id}/>
                     </div>
                   </td>
                 </tr>
