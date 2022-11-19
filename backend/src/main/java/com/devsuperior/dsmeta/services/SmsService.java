7418438f0e91e81e7fb222de3ec066055b9250b2
zm1hawk <vinicius6.lopes6@gmail.com>
2022-11-19 17:18:26 -0300

    Teste SMS3

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -40,11 +40,11 @@ public class SmsService {
 //				" foi destaque em " + date +
 //				" com um total de R$ " + String.format(" %.2f", sale.getAmount());
 		
-//		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-//			    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
-		
 		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
+			    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
+		
+//		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+//			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
